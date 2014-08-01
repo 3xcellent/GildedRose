59eb3ab49f48a5c7f97c3155f88c3723c99914c1
Mark Borcherding <markborcherding@gmail.com>
2014-07-31 19:24:25 -0500

    adding tweaks for rspec 3

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require './gilded_rose.rb'
 
-Rspec.describe GildedRose do
+RSpec.describe GildedRose do
 
   describe "#update_quality" do
 
@@ -15,64 +15,64 @@ Rspec.describe GildedRose do
       context "normal item" do
         let(:name) { "NORMAL ITEM" }
 
-        it { item.sell_in.should == initial_sell_in-1 }
+        it { expect(item.sell_in).to eq(initial_sell_in-1) }
 
         context "before sell date" do
-          it { item.quality.should == initial_quality-1 }
+          it { expect(item.quality).to eq(initial_quality-1) }
         end
 
         context "on sell date" do
           let(:initial_sell_in) { 0 }
-          it { item.quality.should == initial_quality-2 }
+          it { expect(item.quality).to eq(initial_quality-2) }
         end
 
         context "after sell date" do
           let(:initial_sell_in) { -10 }
-          it { item.quality.should == initial_quality-2 }
+          it { expect(item.quality).to eq(initial_quality-2) }
         end
 
         context "of zero quality" do
           let(:initial_quality) { 0 }
-          it { item.quality.should == 0 }
+          it { expect(item.quality).to eq(0) }
         end
       end
 
       context "Aged Brie" do
         let(:name) { "Aged Brie" }
 
-        it { item.sell_in.should == initial_sell_in-1 }
+        it { expect(item.sell_in).to eq(initial_sell_in-1) }
 
         context "before sell date" do
-          it { item.quality.should == initial_quality+1 }
+          it { expect(item.quality).to eq(initial_quality+1) }
 
           context "with max quality" do
             let(:initial_quality) { 50 }
-            it { item.quality.should == initial_quality }
+            it { expect(item.quality).to eq(initial_quality) }
           end
         end
 
         context "on sell date" do
           let(:initial_sell_in) { 0 }
-          it { item.quality.should == initial_quality+2 }
+          it { expect(item.quality).to eq(initial_quality+2) }
 
           context "near max quality" do
             let(:initial_quality) { 49 }
-            it { item.quality.should == 50 }
+            it { expect(item.quality).to eq(50) }
           end
 
           context "with max quality" do
             let(:initial_quality) { 50 }
-            it { item.quality.should == initial_quality }
+            it { expect(item.quality).to eq(initial_quality) }
           end
         end
 
         context "after sell date" do
           let(:initial_sell_in) { -10 }
-          it { item.quality.should == initial_quality+2 }
+          it { expect(item.quality).to eq(initial_quality+2) }
 
           context "with max quality" do
             let(:initial_quality) { 50 }
-            it { item.quality.should == initial_quality }
+            it { expect(item.quality).to eq(initial_quality) }
           end
         end
       end
@@ -81,31 +81,31 @@ Rspec.describe GildedRose do
         let(:initial_quality) { 80 }
         let(:name) { "Sulfuras, Hand of Ragnaros" }
 
-        it { item.sell_in.should == initial_sell_in }
+        it { expect(item.sell_in).to eq(initial_sell_in) }
 
         context "before sell date" do
-          it { item.quality.should == initial_quality }
+          it { expect(item.quality).to eq(initial_quality) }
         end
 
         context "on sell date" do
           let(:initial_sell_in) { 0 }
-          it { item.quality.should == initial_quality }
+          it { expect(item.quality).to eq(initial_quality) }
         end
 
         context "after sell date" do
           let(:initial_sell_in) { -10 }
-          it { item.quality.should == initial_quality }
+          it { expect(item.quality).to eq(initial_quality) }
         end
       end
 
       context "Backstage pass" do
         let(:name) { "Backstage passes to a TAFKAL80ETC concert" }
 
-        it { item.sell_in.should == initial_sell_in-1 }
+        it { expect(item.sell_in).to eq(initial_sell_in-1) }
 
         context "long before sell date" do
           let(:initial_sell_in) { 11 }
-          it { item.quality.should == initial_quality+1 }
+          it { expect(item.quality).to eq(initial_quality+1) }
 
           context "at max quality" do
             let(:initial_quality) { 50 }
@@ -114,88 +114,88 @@ Rspec.describe GildedRose do
 
         context "medium close to sell date (upper bound)" do
           let(:initial_sell_in) { 10 }
-          it { item.quality.should == initial_quality+2 }
+          it { expect(item.quality).to eq(initial_quality+2) }
 
           context "at max quality" do
             let(:initial_quality) { 50 }
-            it { item.quality.should == initial_quality }
+            it { expect(item.quality).to eq(initial_quality) }
           end
         end
 
         context "medium close to sell date (lower bound)" do
           let(:initial_sell_in) { 6 }
-          it { item.quality.should == initial_quality+2 }
+          it { expect(item.quality).to eq(initial_quality+2) }
 
           context "at max quality" do
             let(:initial_quality) { 50 }
-            it { item.quality.should == initial_quality }
+            it {expect(item.quality).to eq(initial_quality) }
           end
         end
 
         context "very close to sell date (upper bound)" do
           let(:initial_sell_in) { 5 }
-          it { item.quality.should == initial_quality+3 }
+          it { expect(item.quality).to eq(initial_quality+3) }
 
           context "at max quality" do
             let(:initial_quality) { 50 }
-            it { item.quality.should == initial_quality }
+            it { expect(item.quality).to eq(initial_quality) }
           end
         end
 
         context "very close to sell date (lower bound)" do
           let(:initial_sell_in) { 1 }
-          it { item.quality.should == initial_quality+3 }
+          it { expect(item.quality).to eq(initial_quality+3) }
 
           context "at max quality" do
             let(:initial_quality) { 50 }
-            it { item.quality.should == initial_quality }
+            it { expect(item.quality).to eq(initial_quality) }
           end
         end
 
         context "on sell date" do
           let(:initial_sell_in) { 0 }
-          it { item.quality.should == 0 }
+          it { expect(item.quality).to eq(0) }
         end
 
         context "after sell date" do
           let(:initial_sell_in) { -10 }
-          it { item.quality.should == 0 }
+          it { expect(item.quality).to eq(0) }
         end
       end
 
       context "conjured item" do
-        before { pending "Waiting implementation" }
+        before { skip "Waiting implementation" }
         let(:name) { "Conjured Mana Cake" }
 
-        it { item.sell_in.should == initial_sell_in-1 }
+        it { expect(item.sell_in).to eq(initial_sell_in-1) }
 
         context "before the sell date" do
           let(:initial_sell_in) { 5 }
-          it { item.quality.should == initial_quality-2 }
+          it { expect(item.quality).to eq(initial_quality-2) }
 
           context "at zero quality" do
             let(:initial_quality) { 0 }
-            it { item.quality.should == initial_quality }
+            it { expect(item.quality).to eq(initial_quality) }
           end
         end
 
         context "on sell date" do
           let(:initial_sell_in) { 0 }
-          it { item.quality.should == initial_quality-4 }
+          it { expect(item.quality).to eq(initial_quality-4) }
 
           context "at zero quality" do
             let(:initial_quality) { 0 }
-            it { item.quality.should == initial_quality }
+            it { expect(item.quality).to eq(initial_quality) }
           end
         end
 
         context "after sell date" do
           let(:initial_sell_in) { -10 }
-          it { item.quality.should == initial_quality-4 }
+          it { expect(item.quality).to eq(initial_quality-4) }
 
           context "at zero quality" do
             let(:initial_quality) { 0 }
-            it { item.quality.should == initial_quality }
+            it { expect(item.quality).to eq(initial_quality) }
           end
         end
       end
@@ -211,11 +211,11 @@ Rspec.describe GildedRose do
 
       before { subject.update_quality(items) }
 
-      it { items[0].quality.should == 9 }
-      it { items[0].sell_in.should == 4 }
+      it { expect(items[0].quality).to eq(9) }
+      it { expect(items[0].sell_in).to eq(4) }
 
-      it { items[1].quality.should == 11 }
-      it { items[1].sell_in.should == 2 }
+      it { expect(items[1].quality).to eq(11) }
+      it { expect(items[1].sell_in).to eq(2) }
     end
   end
 end
