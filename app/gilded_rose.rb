755b7a2ce9976102d18217c2bbf989346154eb75
Jason Gregory <magnet-dev+jg@asynchrony.com>
2016-06-21 23:22:13 -0500

    moves iterating over items out of update method; fixes tests to not call in array

--- app/gilded_rose.rb
+++ app/gilded_rose.rb
@@ -3,50 +3,56 @@ require_relative './item.rb'
 class GildedRose
   @items = []
 
-  def update_quality(items)
+  def update_items(items)
     @items = items
 
-    for i in 0..(@items.size - 1)
-      if @items[i].name != 'Aged Brie' && @items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-        if @items[i].quality > 0
-          if @items[i].name != 'Sulfuras, Hand of Ragnaros'
-            @items[i].quality = @items[i].quality - 1
-          end
+    items.each do |item|
+      update_quality item
+    end
+  end
+
+  def update_quality(item)
+    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
+      if item.quality > 0
+        if item.name != 'Sulfuras, Hand of Ragnaros'
+          item.quality = item.quality - 1
         end
-      else
-        if @items[i].quality < 50
-          @items[i].quality = @items[i].quality + 1
-          if @items[i].name == 'Backstage passes to a TAFKAL80ETC concert'
-            if @items[i].sell_in < 11
-              if @items[i].quality < 50
-                @items[i].quality = @items[i].quality + 1
-              end
+      end
+    else
+      if item.quality < 50
+        item.quality = item.quality + 1
+        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+          if item.sell_in < 11
+            if item.quality < 50
+              item.quality = item.quality + 1
             end
-            if @items[i].sell_in < 6
-              if @items[i].quality < 50
-                @items[i].quality = @items[i].quality + 1
-              end
+          end
+          if item.sell_in < 6
+            if item.quality < 50
+              item.quality = item.quality + 1
             end
           end
         end
       end
-      if (@items[i].name != 'Sulfuras, Hand of Ragnaros')
-        @items[i].sell_in = @items[i].sell_in - 1
-      end
-      if @items[i].sell_in < 0
-        if @items[i].name != 'Aged Brie'
-          if @items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-            if @items[i].quality > 0
-              if (@items[i].name != 'Sulfuras, Hand of Ragnaros')
-                @items[i].quality = @items[i].quality - 1
-              end
+    end
+
+    if (item.name != 'Sulfuras, Hand of Ragnaros')
+      item.sell_in = item.sell_in - 1
+    end
+
+    if item.sell_in < 0
+      if item.name != 'Aged Brie'
+        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+          if item.quality > 0
+            if (item.name != 'Sulfuras, Hand of Ragnaros')
+              item.quality = item.quality - 1
             end
-          else
-            @items[i].quality = @items[i].quality - @items[i].quality
           end
         else
-          @items[i].quality = @items[i].quality + 1 if @items[i].quality < 50
+          item.quality = item.quality - item.quality
         end
+      else
+        item.quality = item.quality + 1 if item.quality < 50
       end
     end
   end
