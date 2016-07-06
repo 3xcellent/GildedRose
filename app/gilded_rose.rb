8f198ebe8e9c0a05bd2e0a1d0770c1bcdc2f970a
Jason Gregory <magnet-dev+jg@asynchrony.com>
2016-07-06 18:39:37 -0500

    moves out logic for backstage_pass

--- app/gilded_rose.rb
+++ app/gilded_rose.rb
@@ -13,26 +13,15 @@ end
 class ItemUpdater
   def self.update(item)
     return item if item.name == 'Sulfuras, Hand of Ragnaros'
+    return self.backstage_pass(item) if item.name == 'Backstage passes to a TAFKAL80ETC concert'
 
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
+    if item.name != 'Aged Brie'
       if item.quality > 0
         item.quality = item.quality - 1
       end
     else
       if item.quality < 50
         item.quality = item.quality + 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-        end
       end
     end
 
@@ -40,17 +29,37 @@ class ItemUpdater
 
     if item.sell_in < 0
       if item.name != 'Aged Brie'
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            item.quality = item.quality - 1
-          end
-        else
-          item.quality = item.quality - item.quality
+        if item.quality > 0
+          item.quality = item.quality - 1
         end
       else
         item.quality = item.quality + 1 if item.quality < 50
       end
     end
+
+    item
+  end
+
+  def self.backstage_pass(item)
+    if item.quality < 50
+      item.quality = item.quality + 1
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        if item.sell_in < 11
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+        if item.sell_in < 6
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+      end
+    end
+
+    item.sell_in = item.sell_in - 1
+
+    item.quality = item.quality - item.quality if item.sell_in < 0
 
     item
   end
